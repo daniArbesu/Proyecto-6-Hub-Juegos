7383114e3315f62a62cc3c292dd115fa2d28935e
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2023-01-04 12:27:48 +0100

    feat: add new tictactoe test

--- src/utils/tictactoe.test.js
+++ src/utils/tictactoe.test.js
@@ -3,7 +3,7 @@ import { PLAYERS, WINNER_BOARDS_ROW_TEST } from '../constants/tictactoe';
 import checkWinner from './tictactoe';
 
 describe('Winning combinations', () => {
-  it('returns true when a row has a winner', () => {
+  it('returns the right winner when a row has a winner', () => {
     const results = WINNER_BOARDS_ROW_TEST.map((board) => {
       const winner = checkWinner(board, PLAYERS.X);
       return winner === PLAYERS.X;
