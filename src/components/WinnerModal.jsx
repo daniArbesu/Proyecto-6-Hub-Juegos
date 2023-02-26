5450775668d606c7165e19365ba4eb104f0049e6
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2023-02-26 15:02:59 +0100

    fix: correct backdrop on modal

--- src/components/WinnerModal.jsx
+++ src/components/WinnerModal.jsx
@@ -5,14 +5,11 @@ const Modal = styled.dialog`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  padding: 30px;
+  padding: 40px;
   border-radius: 8px;
   border: 1px solid #888;
   background-color: #ffffffe2;
-
-  ::backdrop {
-    background: rgba(0, 0, 0, 0.655);
-  }
+  box-shadow: 0 0 0 100vw rgba(0, 0, 0, 0.8);
 `;
 
 function WinnerModal({ winner, resetGame }) {
