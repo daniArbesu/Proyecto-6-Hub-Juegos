c7ada9aa07a3978a8057f65ce9dfb00c7de84a82
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2023-03-10 13:45:14 +0100

    fix: correct wrong styling

--- src/pages/Sudoku.jsx
+++ src/pages/Sudoku.jsx
@@ -1,26 +1,21 @@
 import { useState } from 'react';
-import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 import SudokuBoard from '../components/SudokuBoard';
-
-const SudokuPageWrapper = styled.section`
-  display: flex;
-  text-align: ;
-  max-width: 400px;
-`;
 
 function Sudoku() {
   const [isStarted, setIsStarted] = useState(false);
 
   return (
-    <SudokuPageWrapper>
+    <section>
       <article>
         <h2>Play Sudoku</h2>
+        <Link to="/">Back</Link>
         <button type="button" onClick={() => setIsStarted(!isStarted)}>
           {isStarted ? 'End Game' : 'Start Game'}
         </button>
         {isStarted ? <SudokuBoard /> : null}
       </article>
-    </SudokuPageWrapper>
+    </section>
   );
 }
 
