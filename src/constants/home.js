ecd9160cd78eab45b0587ad2213565c40eef554c
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2023-03-18 11:03:52 +0100

    feat: add card to home page

---
+++ src/constants/home.js
@@ -0,0 +1,16 @@
+const links = [
+  {
+    url: '/tictactoe',
+    heading: 'Tic - Tac - Toe'
+  },
+  {
+    url: '/hangman',
+    heading: 'Hangman'
+  },
+  {
+    url: '/sudoku',
+    heading: 'Sudoku'
+  }
+];
+
+export default links;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,25 @@
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import Card from '../components/ui/Card';
+import links from '../constants/home';
+
+const CardsWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
+`;
 
 function Home() {
   return (
     <div>
       <h1>Select a game</h1>
-      <nav>
-        <Link to="/">Home | </Link>
-        <Link to="/tictactoe">Tic-Tac-Toe | </Link>
-        <Link to="/hangman">Hangman | </Link>
-        <Link to="/sudoku">Sudoku | </Link>
-      </nav>
+      <CardsWrapper>
+        {links.map(({ url, heading }) => (
+          <Link to={url} key={url}>
+            <Card heading={heading} />
+          </Link>
+        ))}
+      </CardsWrapper>
     </div>
   );
 }
